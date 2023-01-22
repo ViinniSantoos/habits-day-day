bc123ca9db414ed49c8928bf131e6190d7c3a86c
Eduardo Vinicius <e.vinicius.s.c@gmail.com>
2023-01-22 14:22:30 -0300

    add new days

--- scrip.js
+++ scrip.js
@@ -6,6 +6,15 @@ button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add() {
+  nlwSetup.addDay("23/01")
+  nlwSetup.addDay("24/01")
+  nlwSetup.addDay("25/01")
+  nlwSetup.addDay("26/01")
+  nlwSetup.addDay("27/01")
+  nlwSetup.addDay("28/01")
+  nlwSetup.addDay("29/01")
+  nlwSetup.addDay("30/01")
+  nlwSetup.addDay("31/01")
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
 
